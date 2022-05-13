ab2ef1e1d61ddeeedcd01243e5a9e68037fa9e91
Rafał Kowalewski <mrrafkow@gmail.com>
2022-05-13 20:43:33 +0200

    2ndary commit

--- script1.js
+++ script1.js
@@ -1,80 +1,98 @@
 //menu buttons
-var startButton = document.querySelector('.start');
-var stopButton = document.querySelector('.stop');
-var resumeButton = document.querySelector('.resume');
+const startButton = document.querySelector('.start');
+const stopButton = document.querySelector('.stop');
+const resumeButton = document.querySelector('.resume');
 
 //sliders
-var volume = document.getElementById('volume');
-var panner = document.getElementById('panner');
+const volume = document.getElementById('volume');
+const panner = document.getElementById('panner');
 
 //audio
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var oscillator = audioCtx.createOscillator();
-var waveShape = oscillator.type;
-var gain = audioCtx.createGain();
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+const oscillator = audioCtx.createOscillator();
+const waveShape = oscillator.type;
+const gain = audioCtx.createGain();
 
 //diplay
-var displayHz = document.querySelector('.displayHz');
+const displayHz = document.querySelector('.displayHz');
 displayHz.innerHTML = panner.value + 'Hz'
-var displayShape = document.querySelector('.displayShape');
+const displayShape = document.querySelector('.displayShape');
 displayShape.innerHTML = waveShape;
-var flag = true;
+const flag = true;
 
 //magic
 gain.gain.value = volume.value; //gain gets volume from slider
 oscillator.connect(gain);
 gain.connect(audioCtx.destination);
 
-var increment = function () {
-    if(oscillator.frequency.value < 1000 && flag == true)
-    {
-    oscillator.frequency.value += 20;
-    displayHz.innerHTML = oscillator.frequency.value + 'Hz'}
-    else if (flag == true){
-       setTimeout(pauseAudio, 2000)
+const increment = function () {
+    if (oscillator.frequency.value < 1000 && flag == true) {
+        oscillator.frequency.value += 20;
+        displayHz.innerHTML = oscillator.frequency.value + 'Hz'
+    } else if (flag == true) {
+        setTimeout(pauseAudio, 2000)
         flag = false;
     }
 }
-var stopAudio = () => {
+const stopAudio = () => {
     flag = false;
     clearInterval(startAudio);
 }
-var startAudio = () => {
+const startAudio = () => {
     document.querySelector('.start').style.display = 'none';
     oscillator.start();
-    setInterval(increment, 100)    
+    setInterval(increment, 100)
 }
-var pauseAudio = () => {
-    clearTimeout(increment);
-    audioCtx.suspend();
-    flag = false;
+const pauseAudio = () => {
+        clearTimeout(increment);
+        audioCtx.suspend();
+        flag = false;
 
-}
+    },
+    resumeAudio = () => {
+        if (flag == false) {
+            audioCtx.resume();
+            flag = true;
+        } else {
+            audioCtx.resume();
+            flag = false;
+        }
+
+
+    }
 
 //buttonsActions
 startButton.addEventListener('click', startAudio);
-stopButton.addEventListener('click', pauseAudio);   
-resumeButton.addEventListener('click', function () { audioCtx.resume();
-    flag = true;
-})
-
+stopButton.addEventListener('click', pauseAudio);
+resumeButton.addEventListener('click', resumeAudio);
 //shapeMenuActions
 document.querySelectorAll("div.shapeMenu form input[name='shape']").forEach(element => {
-element.addEventListener('input', function() {
-    oscillator.type = element.value;
-    displayShape.innerHTML = oscillator.type
-}
-    ) 
-}
+        element.addEventListener('input', function () {
+            oscillator.type = element.value;
+            displayShape.innerHTML = oscillator.type
+        })
+    }
 
 );
 //slidersActions
-volume.addEventListener('input', function() {
+volume.addEventListener('input', function () {
     gain.gain.value = this.value; //refresh value after input
 });
 panner.addEventListener('input', function () {
-        oscillator.frequency.value = this.value;
-        displayHz.innerHTML = this.value + 'Hz'; //refresh value after input
+    oscillator.frequency.value = this.value;
+    displayHz.innerHTML = this.value + 'Hz'; //refresh value after input
 });
-
-document.addEventListener("DOMContentLoaded", startAudio());''
+window.addEventListener('keyown', function a(e) {
+    if (e.keyCode == 32 && flag == false) {
+        resumeAudio();
+        flag = true;
+    } else if (e.keyCode == 32 && flag == true) {
+        pauseAudio();
+        flag = false;
+    }
+});
+document.addEventListener("DOMContentLoaded", startAudio());
+''
+window.addEventListener('keydown', function b(e) {
+    console.log(e.keyCode)
+})
